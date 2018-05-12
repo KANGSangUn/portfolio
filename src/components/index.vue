59742520da9814441118fabddc7f19afd3081611
KANGSangUn <rkdtkddns23@naver.com>
2018-05-12 18:12:49 +0900

    #

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="main-page-view">
-      <div>
-        asd
+      <div class="main-page-view-sub-1">
+       <div class="menu-contain">a</div>
+       <div class="menu-contain">b</div>
+       <div class="menu-contain">c</div>
+       <div class="menu-contain">d</div>
+       <div class="menu-contain">e</div>
       </div>
       <div>
         Hello World!
@@ -15,7 +19,7 @@ export default {};
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-@import url("https://fonts.googleapis.com/css?family=Shadows+Into+Light+Two");
+@import url("https://fonts.googleapis.com/css?family=Nanum+Gothic");
 .main-page-view {
   height: 100vh;
   display: grid;
@@ -23,15 +27,25 @@ export default {};
 }
 
 .main-page-view > div:nth-child(1) {
+  display: grid;
+  grid-template-rows: repeat(5, 20%);
+}
+.menu-contain {
   background: blue;
+  width: 100%;
+  transition: width 0.5s;
+}
+.menu-contain:hover {
+  width: 200%;
+}
+.menu-contain:hover ~ .menu-contain {
+  width: 50%;
 }
 
 .main-page-view > div:nth-child(2) {
   padding: 10%;
   text-align: right;
-  font-family: "Shadows Into Light Two", cursive;
+  font-family: "Nanum Gothic", sans-serif;
   font-size: 7vw;
-  color: white;
-  background: #34314c;
 }
 </style>
